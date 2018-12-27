fb88873b00ba6959c0b7762b6b7bc88598785e9a
xuxia <1786425454@qq.com>
2018-12-27 10:47:09 +0800

    react+react-redux(装饰器写法)+router 结合使用的基本用法

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-export default class App extends React.Component{
-  // constructor(props){
-  //   super(props);
-  // }
-
+import { connect } from 'react-redux';
+import {addGUN, removeGUN, addGunAsync} from './index.redux';
+// 使用装饰器的写法
+@connect(
+  // 你要state什么属性放到props里
+  state => ({num: state}),
+  // 你要什么方法，放到props里，自动dispatch
+  { addGUN, removeGUN, addGunAsync }
+)
+class App extends React.Component{
   render(){
-    const {store, addGUN, removeGUN, addGunAsync} = this.props;
-    const num = store.getState();
+    const {addGUN, removeGUN, addGunAsync, num} = this.props;
     return (
       <div>
         <h1>现在有机枪{num}把</h1>
-        <button onClick={()=>store.dispatch(addGUN())}>申请武器</button>
-        <button onClick={()=>store.dispatch(removeGUN())}>上交武器</button>
-        <button onClick={()=>store.dispatch(addGunAsync())}>晚两天给你</button>
+        <button onClick={addGUN}>申请武器</button>
+        <button onClick={removeGUN}>上交武器</button>
+        <button onClick={addGunAsync}>晚两天给你</button>
       </div>
     )
   }
 }
+// 不使用装饰器的写法
+// const mapStateToProps=(state)=>{
+//   return {num: state}
+// }
+// const mapDispatchToProps = { addGUN, removeGUN, addGunAsync }
+// App = connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,38 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 import App from './App';
-import {counter, addGUN, removeGUN,addGunAsync} from './index.redux';
+import { counter } from './index.redux';
 
 const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : ()=>{};
 const store = createStore(counter,compose(
   applyMiddleware(thunk),
   reduxDevtools
 ));
-
-function render(){
-  const appProps={
-    store: store,
-    addGUN: addGUN,
-    removeGUN: removeGUN,
-    addGunAsync: addGunAsync
-  }
-  ReactDom.render(<App {...appProps} />,document.getElementById('root'))
+function Erying(){
+  return <h2>二营</h2>
 }
-render();
-store.subscribe(render);
+function Qibinglian(){
+  return <h2>骑兵连</h2>
+}
+ReactDom.render(
+  (<Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <ul>
+          {/* 点击跳到指定路由 */}
+          <li><Link to='/'>一营</Link></li>
+          <li><Link to='/erying'>二营</Link></li>
+          <li><Link to='/qibinglian'>骑兵连</Link></li>
+        </ul>
+        {/* 路由对应渲染模板 exact代码完全匹配*/}
+        <Route path='/' exact component={App}></Route>
+        <Route path='/erying' component={Erying}></Route>
+        <Route path='/qibinglian' component={Qibinglian}></Route>
+      </div>
+    </BrowserRouter>
+  </Provider>)
+  ,document.getElementById('root'))
